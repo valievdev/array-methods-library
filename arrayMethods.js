81279630232e41369ca946542665b546b781d5df
azat valiev <azvaliev@gmail.com>
2022-05-19 07:40:36 -0700

    initial empty

---
+++ arrayMethods.js
@@ -0,0 +1,34 @@
+function forEach(array, cb) {
+}
+
+function map(array, cb) {
+}
+
+function filter(array, cb) {
+}
+
+function reduce(array, cb, initialValue) {
+}
+
+function some(array, cb) {
+}
+
+function every(array, cb) {
+}
+
+function flat(array, depth = 1) {
+}
+
+function find(array, cb) {
+}
+
+module.exports = {
+  forEach,
+  map,
+  filter,
+  reduce,
+  some,
+  every,
+  flat,
+  find,
+}
